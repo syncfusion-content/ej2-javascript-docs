68ffde4b995cf79372ece32692dcd29219374843
Build Automaion <buildautomation@syncfusion.com>
2023-07-15 14:58:32 +0530

    Integrated latest changes at 07-15-2023 1:30:51 PM

---
+++ ej2-javascript/code-snippet/common/material3-cs1/index.ts
@@ -0,0 +1,20 @@
+
+
+import { Button } from '@syncfusion/ej2-buttons';
+
+var checkBoxObj = new Button.CheckBox({ label: 'Enable DarkMode', change: onDarkMode });
+checkBoxObj.appendTo('#darkmode');
+
+function onDarkMode(e) {
+    e.checked ? document.body.classList.add('e-dark-mode') : document.body.classList.remove('e-dark-mode');
+}
+var btnObj = new Button({ isPrimary: true });
+    btnObj.appendTo('#normal4');
+    btnObj = new Button({ cssClass: 'e-success' });
+    btnObj.appendTo('#normal5');
+    btnObj = new Button({ cssClass: 'e-info' });
+    btnObj.appendTo('#normal6');
+    btnObj = new Button({ cssClass: 'e-warning' });
+    btnObj.appendTo('#normal7');
+    btnObj = new Button({ cssClass: 'e-danger' });
+    btnObj.appendTo('#normal8');

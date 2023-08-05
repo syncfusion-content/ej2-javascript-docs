93657959b0b3241db069fb2767c3bc8b517e54f9
Build Automaion <buildautomation@syncfusion.com>
2023-08-05 10:56:25 +0530

    Integrated latest changes at 08-05-2023 10:30:06 AM

--- ej2-javascript/code-snippet/chart/chart-types-cs24/index.js
+++ ej2-javascript/code-snippet/chart/chart-types-cs24/index.js
@@ -2,27 +2,22 @@ var piechart = new ej.charts.AccumulationChart({
     series: [
         {
             dataSource: [
-                { x: 'Jan', y: 3, text: 'Jan: 3' }, { x: 'Feb', y: 3.5, text: 'Feb: 3.5' },
-                { x: 'Mar', y: 7, text: 'Mar: 7' }, { x: 'Apr', y: 13.5, text: 'Apr: 13.5' },
-                { x: 'May', y: 19, text: 'May: 19' }, { x: 'Jun', y: 23.5, text: 'Jun: 23.5' },
-                { x: 'Jul', y: 26, text: 'Jul: 26' }, { x: 'Aug', y: 25, text: 'Aug: 25' },
-                { x: 'Sep', y: 21, text: 'Sep: 21' }, { x: 'Oct', y: 15, text: 'Oct: 15' },
-                { x: 'Nov', y: 9, text: 'Nov: 9' }, { x: 'Dec', y: 3.5, text: 'Dec: 3.5' }],
-            dataLabel: { visible: true, name: 'text', position: 'Outside' },
-            groupTo: '11',
+                { x: 'Renewed', y: 18.20, text: 'Renewed 18.20%' }, { x: 'Subscribe', y: 27.3, text: 'Subscribe 27.3%' },
+                { x: 'Support', y: 55.9, text: 'Support 55.9%' },{ x: 'Downloaded', y: 76.8, text: 'Downloaded 76.8%' },
+                { x: 'Visited', y: 100, text: 'Visited 100%' }
+            ],
+            type: 'Funnel',
+            gapRatio: 0.08,
             xName: 'x',
             yName: 'y'
         }
     ],
-    textRender: (args) => {
-        if (args.text.indexOf('Others') > -1) {
-            args.color = 'red';
-            args.border.width = 1;
-        }
-    },
     pointRender: (args) => {
-        if ((args.point.x).indexOf('Others') > -1) {
-            args.fill = '#D3D3D3';
+        if ((args.point.x).indexOf('Downloaded') > -1) {
+            args.fill = '#f4bc42';
+        }
+        else {
+            args.fill = '#597cf9';
         }
     },
 }, '#element');

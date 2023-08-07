ce3313b331145c75aa0d77d572c86b768cb989f6
Build Automaion <buildautomation@syncfusion.com>
2023-08-07 14:38:42 +0530

    Integrated latest changes at 08-07-2023 1:30:19 PM

---
+++ ej2-javascript/code-snippet/document-editor/save-aws-s3/index.ts
@@ -0,0 +1,42 @@
+
+import { DocumentEditorContainer, Toolbar } from '@syncfusion/ej2-documenteditor';
+
+// Inject require modules.
+DocumentEditorContainer.Inject(Toolbar);
+
+//Initialize Document Editor component
+let documentEditorContainer: DocumentEditorContainer = new DocumentEditorContainer({ enableToolbar: true, height: '590px', serviceUrl: 'https://ej2services.syncfusion.com/production/web-services/api/documenteditor/' });
+
+// Render Document Editor component.
+documentEditorContainer.appendTo('#DocumentEditor');
+
+document.getElementById('export').addEventListener('click', (): void => {
+    save();
+});
+
+async function save(): Promise<void> {
+    documentEditorContainer.documentEditor.saveAsBlob('Docx').then((blob: Blob) => {
+        let exportedDocument = blob;
+        //Now, save the document where ever you want.
+        let formData: FormData = new FormData();
+        formData.append('fileName', 'sample.docx');
+        formData.append('data', exportedDocument);
+        /* tslint:disable */
+        let req = new XMLHttpRequest();
+        // Replace your running Url here
+        req.open(
+            'POST',
+            'http://localhost:62870/api/documenteditor/SaveToS3',
+            true
+        );
+        req.onreadystatechange = () => {
+            if (req.readyState === 4) {
+                if (req.status === 200 || req.status === 304) {
+                    console.log('Saved sucessfully');
+                }
+            }
+        };
+        req.send(formData);
+    });
+}
+

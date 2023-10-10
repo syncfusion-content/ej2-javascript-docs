282fa8de8edf046a582f8e52237b7c51ec369556
Build Automaion <buildautomation@syncfusion.com>
2023-10-10 21:29:47 +0530

    Integrated latest changes at 10-10-2023 7:23:41 PM

--- ej2-javascript/code-snippet/slider/buttons-cs1/index.ts
+++ ej2-javascript/code-snippet/slider/buttons-cs1/index.ts
@@ -8,7 +8,7 @@ import { Slider } from '@syncfusion/ej2-inputs';
         // Slider tooltip
         tooltip: { placement: 'After', isVisible: true, showOn: 'Always' },
         value: 30,
-        type: 'MinRange'
+        type: 'MinRange',
         showButtons: true
     });
     // Render initialized Slider

f7bb69792166e4184eedd01f810dba88cabea769
Build Automaion <buildautomation@syncfusion.com>
2023-12-20 11:19:30 +0530

    Integrated latest changes at 12-20-2023 10:32:09 AM

--- ej2-javascript/code-snippet/image-editor/straightening-cs1/index.ts
+++ ej2-javascript/code-snippet/image-editor/straightening-cs1/index.ts
@@ -5,28 +5,13 @@ import { Button } from '@syncfusion/ej2-buttons';
 import { Browser } from '@syncfusion/ej2-base';
 
 //Image Editor items definition
- let straighten: number = 0;
  let imageEditorObj: ImageEditor = new ImageEditor({
     width: '550px',
     height: '330px',
-    toolbar: [],
     created: () => {
         imageEditorObj.open('bee-eater.png');
         }
     });
     imageEditorObj.appendTo('#imageeditor');
-
-    document.getElementById('straightenLeft').onclick = (): void => {
-        straighten -= 3;
-        imageEditorObj.straightenImage(straighten);
-    }
-    document.getElementById('straightenRight').onclick = (): void => {
-        straighten += 3;
-        imageEditorObj.straightenImage(straighten);
-    }
-    document.getElementById('reset').onclick = (): void => {
-        straighten = 0;
-        imageEditorObj.straightenImage(straighten);
-    }
 
 

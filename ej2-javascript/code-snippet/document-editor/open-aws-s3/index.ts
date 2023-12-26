acc8813536415549310409f54e2aa82635bf2ba5
Build Automaion <buildautomation@syncfusion.com>
2023-12-26 17:58:05 +0530

    Integrated latest changes at 12-26-2023 5:20:50 PM

--- ej2-javascript/code-snippet/document-editor/open-aws-s3/index.ts
+++ ej2-javascript/code-snippet/document-editor/open-aws-s3/index.ts
@@ -5,27 +5,36 @@ import { DocumentEditorContainer, Toolbar } from '@syncfusion/ej2-documenteditor
 DocumentEditorContainer.Inject(Toolbar);
 
 //Initialize Document Editor component
-let documentEditorContainer: DocumentEditorContainer = new DocumentEditorContainer({ enableToolbar: true, height: '590px', serviceUrl: 'https://services.syncfusion.com/js/production/api/documenteditor/' });
+let documentEditorContainer: DocumentEditorContainer = new DocumentEditorContainer({ enableToolbar: true, height: '590px', serviceUrl: 'http://localhost:62870/api/documenteditor/' });
 
 // Render Document Editor component.
 documentEditorContainer.appendTo('#DocumentEditor');
 
 document.getElementById('load').addEventListener('click', (): void => {
-    load();
+  load();
 });
 
-async function load(): Promise<void> {
-    const response = await fetch(
-      'http://localhost:62870/api/documenteditor/LoadFromS3',
-      {
-        method: 'Get',
-        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
-      }
-    );
-  
-    if (response.status === 200 || response.status === 304) {
-      var json = await response.json();
-      documentEditorContainer.documentEditor.open(JSON.stringify(json));
+function load(): void {
+  fetch(
+    'http://localhost:62870/api/documenteditor/LoadFromS3',
+    {
+      method: 'Post',
+      headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+      body: JSON.stringify({ documentName: 'Getting Started.docx' })
     }
-  }
+  )
+    .then(response => {
+      if (response.status === 200 || response.status === 304) {
+        return response.json(); // Return the Promise
+      } else {
+        throw new Error('Error loading data');
+      }
+    })
+    .then(json => {
+      documentEditorContainer.documentEditor.open(JSON.stringify(json));
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
 

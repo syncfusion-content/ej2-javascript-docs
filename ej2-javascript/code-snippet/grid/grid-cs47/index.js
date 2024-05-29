c77df36b0341972ad226c13fc21f7d45d93673eb
Build Automaion <buildautomation@syncfusion.com>
2024-05-29 12:44:08 +0530

    Integrated latest changes at 05-29-2024 10:30:28 AM

--- ej2-javascript/code-snippet/grid/grid-cs47/index.js
+++ ej2-javascript/code-snippet/grid/grid-cs47/index.js
@@ -9,10 +9,9 @@ var grid = new ej.grids.Grid({
     columns: [
         { field: 'OrderID', headerText: 'Order ID', textAlign: 'Right', width: 100, isPrimaryKey: true, validationRules: { required: true } },
         { field: 'CustomerID', headerText: 'Customer ID', width: 120, validationRules: { required: true, minLength: [customFn, 'Need atleast 5 letters'] } },
-        { field: 'Freight', headerText: 'Freight', textAlign: 'Right', editType: 'numericedit', width: 120, format: 'C2' },
+        { field: 'Freight', headerText: 'Freight', textAlign: 'Right', editType: 'numericedit', validationRules: { required: true, min:1, max:1000 }, width: 120, format: 'C2' },
         { field: 'ShipCountry', headerText: 'Ship Country', editType: 'dropdownedit', width: 150 }
     ],
     height: 273
 });
 grid.appendTo('#Grid');
-

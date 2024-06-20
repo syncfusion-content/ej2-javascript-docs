2b620e04371e4f7f809ae7692c8690b3e274fe4f
Build Automaion <buildautomation@syncfusion.com>
2024-06-20 11:26:59 +0530

    Integrated latest changes at 06-20-2024 10:30:27 AM

--- ej2-javascript/code-snippet/chart/working-with-data-cs9/index.ts
+++ ej2-javascript/code-snippet/chart/working-with-data-cs9/index.ts
@@ -2,9 +2,9 @@ import { Chart, ColumnSeries, Category } from '@syncfusion/ej2-charts';
 import { DataManager, Query, ODataAdaptor } from '@syncfusion/ej2-data';
 Chart.Inject(ColumnSeries, Category);
 let dataManager: DataManager = new DataManager({
-    url: 'https://services.odata.org/V3/Northwind/Northwind.svc/Orders/',
+    url: 'https://services.syncfusion.com/js/production/api/orders',
     adaptor: new ODataAdaptor(),
-    crossDomain: true
+    offline: true
 });
 let query: Query = new Query();
 

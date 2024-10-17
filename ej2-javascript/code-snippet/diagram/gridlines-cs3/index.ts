7fb568c7b2652946812b83838a3fbd797845b967
Build Automaion <buildautomation@syncfusion.com>
2024-10-17 16:02:12 +0530

    Integrated latest changes at 10-17-2024 1:30:54 PM

--- ej2-javascript/code-snippet/diagram/gridlines-cs3/index.ts
+++ ej2-javascript/code-snippet/diagram/gridlines-cs3/index.ts
@@ -1,34 +1,26 @@
 import {
-    Diagram,
-    SnapConstraints,
-    SnapSettingsModel,
-    Snapping,
-  } from '@syncfusion/ej2-diagrams';
-  Diagram.Inject(Snapping);
-  let snapSettings: SnapSettingsModel = {
-    // Define the Constraints for gridlines and snapping
-    constraints: SnapConstraints.ShowLines,
-    gridType: 'Dots',
-    // Defines the horizontalGridlines for SnapSettings
-    horizontalGridlines: {
-      // Sets the line color of gridlines
-      lineColor: 'blue',
-      // Defines the dot intervals of gridlines
-      dotIntervals: [3, 20, 1, 20, 1, 20, 1, 20, 1, 20],
-    },
-    // Defines the verticalGridlines for SnapSettings
-    verticalGridlines: {
-      // Defines the dot intervals of gridlines
-      dotIntervals: [3, 20, 1, 20, 1, 20],
-      // Sets the line color of gridlines
-      lineColor: 'blue',
-    },
-  };
-  let diagram: Diagram = new Diagram({
-    width: '100%',
-    height: '700px',
-    // Define the snap setting for the diagram
-    snapSettings: snapSettings,
-  });
-  diagram.appendTo('#element');
-  
+  Diagram,
+  SnapConstraints,
+  SnapSettingsModel,
+  Snapping,
+} from '@syncfusion/ej2-diagrams';
+Diagram.Inject(Snapping);
+let snapSettings: SnapSettingsModel = {
+  constraints: SnapConstraints.ShowLines,
+  horizontalGridlines: {
+    // Sets the lineIntervals of Gridlines
+    lineIntervals: [1.25, 10, 0.25, 10, 0.25, 10, 0.25, 10, 0.25, 10],
+    lineColor: 'black',
+  },
+  verticalGridlines: {
+    // Sets the lineIntervals of Gridlines
+    lineIntervals: [1.25, 10, 0.25, 10, 0.25, 10, 0.25, 10, 0.25, 10],
+    lineColor: 'black',
+  },
+};
+let diagram: Diagram = new Diagram({
+  width: '100%',
+  height: '500px',
+  snapSettings: snapSettings,
+});
+diagram.appendTo('#element');

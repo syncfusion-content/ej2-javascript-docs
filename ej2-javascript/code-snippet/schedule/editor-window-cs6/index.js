f0607bfbb4636400f3d8229ad455df4268f76ab7
Build Automaion <buildautomation@syncfusion.com>
2025-01-09 12:01:39 +0530

    Integrated latest changes at 01-09-2025 10:30:09 AM

--- ej2-javascript/code-snippet/schedule/editor-window-cs6/index.js
+++ ej2-javascript/code-snippet/schedule/editor-window-cs6/index.js
@@ -1,5 +1,3 @@
-import { scheduleData } from './es5-datasource.js';
-
 var scheduleObj = new ej.schedule.Schedule({
     width: '100%', height: '550px',
     views: ['Day', 'Week', 'WorkWeek', 'Month'],

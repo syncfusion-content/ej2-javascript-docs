fef3b40718223ff71c957c8acbd63e8c02ddcd8c
Build Automaion <buildautomation@syncfusion.com>
2025-07-09 10:46:06 +0530

    Integrated latest changes at 07-09-2025 10:30:08 AM

--- ej2-javascript/code-snippet/data/getting-started-cs34/index.js
+++ ej2-javascript/code-snippet/data/getting-started-cs34/index.js
@@ -1,22 +1,18 @@
+let template = '<tr><td>${OrderID}</td><td>${CustomerID}</td><td>${EmployeeID}</td><td>${ShipCity}</td><td>${ShipCountry}</td></tr>';
+
+let compiledFunction = ej.base.compile(template);
+
+let table = document.getElementById('datatable');
+
 const datamanger = new ej.data.DataManager({
     // Use remote server host and port instead of 'xxxx'.
     url: 'https://localhost:xxxx/api/Orders',
     adaptor: new ej.data.UrlAdaptor(),
 });
 
-datamanger.executeQuery(new ej.data.Query().take(10)).then((e) => {
-    const data = e.result.result;
-    const tbody = document.getElementById('table-body');
-    data.forEach((item) => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
-          <td>${item.OrderID}</td>
-          <td>${item.CustomerID}</td>
-          <td>${item.EmployeeID}</td>
-          <td>${item.ShipCity}</td>
-          <td>${item.ShipCountry}</td>
-      `;
-        tbody.appendChild(row);
+datamanger.executeQuery(new Query().take(10)).then((e) => {
+    e.result.result.forEach((data) => {
+        table.appendChild(compiledFunction(data)[0]);
     });
 }).catch(error => {
     console.error("Data fetch failed:", error);

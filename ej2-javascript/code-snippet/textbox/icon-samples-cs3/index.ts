1c12862b7250d1d50f8e319a2ee3755071c8106f
Build Automaion <buildautomation@syncfusion.com>
2025-07-29 14:54:44 +0530

    Integrated latest changes at 07-29-2025 1:30:26 PM

--- ej2-javascript/code-snippet/textbox/icon-samples-cs3/index.ts
+++ ej2-javascript/code-snippet/textbox/icon-samples-cs3/index.ts
@@ -10,7 +10,7 @@ textBoxObj.appendTo('#textbox');
 let iconTextBox: TextBox = new TextBox({
   placeholder: 'Enter Date',
   created: () => {
-    iconTextBox.addIcon('append', 'e-date-icon');
+    iconTextBox.addIcon('append', 'e-icons e-date-range');
   },
 });
 iconTextBox.appendTo('#textboxicon');

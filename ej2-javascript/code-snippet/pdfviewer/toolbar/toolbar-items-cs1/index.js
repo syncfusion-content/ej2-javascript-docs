c5ac4f13d07a46543122cf265efed1f9f4510709
MuralidharanGSF4527 <muralidharan.g@syncfusion.com>
2025-08-22 19:10:30 +0530

    977314: Migrated of PDF Viewer UG Documentation to Document Processing Repo

--- ej2-javascript/code-snippet/pdfviewer/toolbar/toolbar-items-cs1/index.js
+++
@@ -1,8 +0,0 @@
-let pdfviewer: PdfViewer = new PdfViewer({
-    documentPath:'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf',
-    resourceUrl:'https://cdn.syncfusion.com/ej2/25.1.35/dist/ej2-pdfviewer-lib',
-    toolbarSettings: { showTooltip : true, toolbarItems: ['OpenOption']}
-});
-pdfviewer.appendTo('#PdfViewer');
-
-

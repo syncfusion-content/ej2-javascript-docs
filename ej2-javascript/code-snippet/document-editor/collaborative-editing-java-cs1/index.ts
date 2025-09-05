964e2c7d4f702c0c7afa95308e19cef6050e416e
RahulSF4846 <rahul.ravikumar@syncfusion.com>
2025-09-05 15:02:01 +0530

    980128: Removed JS Documentation

--- ej2-javascript/code-snippet/document-editor/collaborative-editing-java-cs1/index.ts
+++
@@ -1,9 +0,0 @@
-import { DocumentEditorContainer, DocumentEditor, CollaborativeEditingHandler} from '@syncfusion/ej2-documenteditor';
-
-//Inject collaborative editing module.
-DocumentEditor.Inject(CollaborativeEditingHandler);
-let container: DocumentEditorContainer = new DocumentEditorContainer({ enableToolbar: true,  height: '590px'});
-container.appendTo('#container');
-
-//Enable collaborative editing in Document Editor.
-container.documentEditor.enableCollaborativeEditing = true;
--- ej2-javascript/code-snippet/document-editor/dialog-cs11/index.js
+++
@@ -1,8 +0,0 @@
-var documenteditor = new ej.documenteditor.DocumentEditor({ enableBordersAndShadingDialog: true, enableSelection: true, enableEditor: true, isReadOnly: false, enableEditorHistory: true });
-
-documenteditor.appendTo('#DocumentEditor');
-documenteditor.editor.insertTable(2,2);
-document.getElementById('dialog').addEventListener('click', function () {
-    documenteditor.showDialog('BordersAndShading');
-});
-

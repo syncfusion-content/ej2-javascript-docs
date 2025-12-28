8b0904b6cdb7fa220005dac76bdf45ee26e80aef
Build Automaion <buildautomation@syncfusion.com>
2025-12-28 10:47:23 +0530

    Integrated latest changes at 12-28-2025 10:30:10 AM

--- ej2-javascript/code-snippet/maskedtextbox/adornments-cs1/index.js
+++ ej2-javascript/code-snippet/maskedtextbox/adornments-cs1/index.js
@@ -1,11 +1,10 @@
-
-    var countryLabel;
-    var maskObj = new ej.inputs.MaskedTextBox({
-        mask: "0000-000-000",
-        promptChar: '#',
-        placeholder: "Enter phone number",
-        floatLabelType: "Auto",
-        prependTemplate: '<span id="user" class="e-icons e-user" title="User"></span><span class="e-input-separator"></span>',
-        appendTemplate: '<span class="e-input-separator"></span><span id="sendIcon" class="e-icons e-send"></span>'
-    });
-    maskObj.appendTo('#mask');
+var countryLabel;
+var maskObj = new ej.inputs.MaskedTextBox({
+    mask: "0000-000-000",
+    promptChar: '#',
+    placeholder: "Enter phone number",
+    floatLabelType: "Auto",
+    prependTemplate: '<span id="user" class="e-icons e-user" title="User"></span><span class="e-input-separator"></span>',
+    appendTemplate: '<span class="e-input-separator"></span><span id="sendIcon" class="e-icons e-send"></span>'
+});
+maskObj.appendTo('#mask');
--- ej2-javascript/code-snippet/maskedtextbox/adornments-cs1/index.ts
+++ ej2-javascript/code-snippet/maskedtextbox/adornments-cs1/index.ts
@@ -1,14 +1,11 @@
-
 import { MaskedTextBox } from '@syncfusion/ej2-inputs';
 
-
-    
-    let maskObj: MaskedTextBox = new MaskedTextBox({
-        mask: "0000-000-000",
-        promptChar: '#',
-        placeholder: "Enter phone number",
-        floatLabelType: "Auto",
-        prependTemplate: '<span id="user" class="e-icons e-user" title="User"></span><span class="e-input-separator"></span>',
-        appendTemplate: '<span class="e-input-separator"></span><span id="sendIcon" class="e-icons e-send"></span>'
-    });
-    maskObj.appendTo('#mask');
+let maskObj: MaskedTextBox = new MaskedTextBox({
+    mask: "0000-000-000",
+    promptChar: '#',
+    placeholder: "Enter phone number",
+    floatLabelType: "Auto",
+    prependTemplate: '<span id="user" class="e-icons e-user" title="User"></span><span class="e-input-separator"></span>',
+    appendTemplate: '<span class="e-input-separator"></span><span id="sendIcon" class="e-icons e-send"></span>'
+});
+maskObj.appendTo('#mask');

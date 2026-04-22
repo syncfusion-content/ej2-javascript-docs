d036b8db930f8e4e8f6ea9a4ebbe4c5f2ffb96d2
Build Automaion <buildautomation@syncfusion.com>
2026-04-22 20:44:36 +0530

    Integrated latest changes at 04-22-2026 1:55:27 PM

--- ej2-javascript/code-snippet/schedule/localization-cs2/index.js
+++ ej2-javascript/code-snippet/schedule/localization-cs2/index.js
@@ -1,38 +1,24 @@
-loadCultureFiles();
-var localeTexts;
-var localeAjax = new ej.base.Ajax('https://helpej2.syncfusion.com/javascript/documentation/code-snippet/schedule/localization-cs2/locale.json', 'GET', false);
-localeAjax.onSuccess = function (value) {
-localeTexts = value;
-};
-localeAjax.send();
-ej.base.L10n.load(JSON.parse(localeTexts));
+import './es5-datasource.js';
+import './locale.js';
+import './ca-gregorian.js';
+import './numberingSystems.js';
+import './numbers.js';
+import './timeZoneNames.js';
 
-function loadCultureFiles() {
-    var files = ['https://helpej2.syncfusion.com/javascript/documentation/code-snippet/schedule/localization-cs2/ca-gregorian.json',
-        'https://helpej2.syncfusion.com/javascript/documentation/code-snippet/schedule/localization-cs2/numbers.json',
-        'https://helpej2.syncfusion.com/javascript/documentation/code-snippet/schedule/localization-cs2/numberingSystems.json',
-        'https://helpej2.syncfusion.com/javascript/documentation/code-snippet/schedule/localization-cs2/timeZoneNames.json'];
-    var loader = ej.base.loadCldr;
-    var loadCulture = function (prop) {
-        var val, ajax;
-        ajax = new ej.base.Ajax(files[prop], 'GET', false);
-        ajax.onSuccess = function (value) {
-            val = value;
-        };
-        ajax.send();
-        loader(JSON.parse(val));
-    };
-    for (var prop = 0; prop < files.length; prop++) {
-        loadCulture(prop);
-    }
-}
+ej.base.L10n.load(window.locale);
 
-var scheduleObj = new ej.schedule.Schedule({
-  width: '100%',
-  height: '550px',
-  locale: 'fr-CH',
-  views: ['Day', 'Week', 'WorkWeek', 'Month'],
-  selectedDate: new Date(2018, 1, 15),
-  eventSettings: { dataSource: scheduleData }
-});
-scheduleObj.appendTo('#Schedule');
+ej.base.loadCldr(
+    window.gregorian,
+    window.numbersData,
+    window.timeZoneNames,
+    window.numberingSystems
+);
+
+new ej.schedule.Schedule({
+    width: '100%',
+    height: '550px',
+    locale: 'fr-CH',
+    views: ['Day', 'Week', 'WorkWeek', 'Month'],
+    selectedDate: new Date(2018, 1, 15),
+    eventSettings: { dataSource: window.scheduleData },
+}).appendTo('#Schedule');

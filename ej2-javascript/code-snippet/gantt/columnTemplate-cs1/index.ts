e94cc0f1efd94f642c031b8a4b4c04dae9943890
Build Automaion <buildautomation@syncfusion.com>
2026-06-02 13:41:08 +0530

    Integrated latest changes at 06-02-2026 1:30:03 PM

--- ej2-javascript/code-snippet/gantt/columnTemplate-cs1/index.ts
+++ ej2-javascript/code-snippet/gantt/columnTemplate-cs1/index.ts
@@ -1,71 +1,45 @@
 import { Gantt } from '@syncfusion/ej2-gantt';
-
-let ProjectResources: Object[] = [
-    { ResourceId: 1, ResourceName: 'Martin Tamer' },
-    { ResourceId: 2, ResourceName: 'Rose Fuller' },
-    { ResourceId: 3, ResourceName: 'Margaret Buchanan' },
-    { ResourceId: 4, ResourceName: 'Fuller King' },
-    { ResourceId: 5, ResourceName: 'Davolio Fuller' },
-    { ResourceId: 6, ResourceName: 'Van Jack' },
-    { ResourceId: 7, ResourceName: 'Fuller Buchanan' },
-    { ResourceId: 8, ResourceName: 'Jack Davolio' },
-    { ResourceId: 9, ResourceName: 'Tamer Vinet' },
-    { ResourceId: 10, ResourceName: 'Vinet Fuller' },
-    { ResourceId: 11, ResourceName: 'Bergs Anton' },
-    { ResourceId: 12, ResourceName: 'Construction Supervisor' }
-];
-
-let GanttData: Object[] = [
-    {
-        TaskID: 1,
-        TaskName: 'Project initiation',
-        StartDate: new Date('04/02/2019'),
-        EndDate: new Date('04/21/2019'),
-    },
-    { TaskID: 2, TaskName: 'Identify site location', StartDate: new Date('04/02/2019'), Duration: 4, ParentID: 1, Progress: 50, Resources: [1] },
-    { TaskID: 3, TaskName: 'Perform soil test', StartDate: new Date('04/02/2019'), Duration: 4, ParentID: 1, Predecessor: '2', Progress: 50, Resources: [2] },
-    { TaskID: 4, TaskName: 'Soil test approval', StartDate: new Date('04/02/2019'), Duration: 4, ParentID: 1, Predecessor: '3', Progress: 50, Resources: [3] },
-    {
-        TaskID: 5,
-        TaskName: 'Project estimation',
-        StartDate: new Date('04/02/2019'),
-        EndDate: new Date('04/21/2019'),
-    },
-    { TaskID: 6, TaskName: 'Develop floor plan for estimation', StartDate: new Date('04/04/2019'), Duration: 3, ParentID: 5, Predecessor: '4', Progress: 50, Resources: [4] },
-    { TaskID: 7, TaskName: 'List materials', StartDate: new Date('04/04/2019'), Duration: 3, ParentID: 5, Predecessor: '6', Resources: [3], Progress: 50 },
-    {
-        TaskID: 8, TaskName: 'Estimation approval', StartDate: new Date('04/04/2019'), Duration: 3, ParentID: 5, Predecessor: '7',
-    }
-];
+import { data, ProjectResources } from './datasource.ts';
 
 let gantt: Gantt = new Gantt({
-    dataSource: GanttData,
+    dataSource: data,
+    height: '450px',
+    rowHeight: 60,
     taskFields: {
         id: 'TaskID',
         name: 'TaskName',
-        resourceInfo: 'Resources',
         startDate: 'StartDate',
         duration: 'Duration',
         progress: 'Progress',
-        parentID: 'ParentID'
+        parentID: 'ParentID',
+        resourceInfo: 'resources'
     },
-    rowHeight: 50,
-    splitterSettings: {
-        columnIndex: 3
-    },
-    height: '450px',
-    columns: [
-        { field: 'TaskID', headerText: 'Task ID', textAlign: 'Left', width: '100' },
-        { field: 'TaskName', headerText: 'Task Name', width: '150' },
-        { field: 'Resources', headerText: 'Resources', width: '250', template: '#columnTemplate' },
-        { field: 'StartDate', headerText: 'Start Date', width: '150' },
-        { field: 'Duration', headerText: 'Duration', width: '150' },
-        { field: 'Progress', headerText: 'Progress', width: '150' },
-    ],
     resourceFields: {
-        id: 'ResourceId',
-        name: 'ResourceName',
+        id: 'resourceID',
+        name: 'resourceName'
     },
-    resources: ProjectResources
+    resources: ProjectResources,
+    splitterSettings: {
+        columnIndex: 7
+    },
+    columns: [
+        { field: 'TaskID' },
+        {
+            field: 'resources',
+            headerText: 'Resources',
+            width: 250,
+            textAlign: 'Center',
+            template: (props: any): string => {
+                return `<div class="image">
+                            <img src="${props.TaskID}.png" style="height:42px" />
+                        </div>`;
+            }
+        },
+        { field: 'TaskName' },
+        { field: 'StartDate' },
+        { field: 'Duration' },
+        { field: 'Progress' }
+    ]
 });
+
 gantt.appendTo('#Gantt');

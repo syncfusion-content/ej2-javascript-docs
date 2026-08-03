91ed397f92acae764cc1b4434a0d79e40c1599e0
Build Automaion <buildautomation@syncfusion.com>
2026-08-04 02:14:41 +0530

    Integrated latest changes at 08-04-2026 1:46:39 AM

--- ej2-javascript/code-snippet/common/getting-started-cs1/index.ts
+++ ej2-javascript/code-snippet/common/getting-started-cs1/index.ts
@@ -8,12 +8,12 @@ import { Grid } from '@syncfusion/ej2-grids';
             ShipRegion: 'CJ', ShipPostalCode: '51100', ShipCountry: 'France', Freight: 32.38, Verified: !0
         },
         {
-            OrderID: 10249, CustomerID: 'TOMSP', EmployeeID: 6, OrderDate: new Date(836505e6),
+            OrderID: 10249, CustomerID: 'TOMSP', EmployeeID: 6, OrderDate: new Date(8367642e5),
             ShipName: 'Toms Spezialitäten', ShipCity: 'Münster', ShipAddress: 'Luisenstr. 48',
             ShipRegion: 'CJ', ShipPostalCode: '44087', ShipCountry: 'Germany', Freight: 11.61, Verified: !1
         },
         {
-            OrderID: 10250, CustomerID: 'HANAR', EmployeeID: 4, OrderDate: new Date(8367642e5),
+            OrderID: 10250, CustomerID: 'HANAR', EmployeeID: 4, OrderDate: new Date(8371242e5),
             ShipName: 'Hanari Carnes', ShipCity: 'Rio de Janeiro', ShipAddress: 'Rua do Paço, 67',
             ShipRegion: 'RJ', ShipPostalCode: '05454-876', ShipCountry: 'Brazil', Freight: 65.83, Verified: !0
         }
